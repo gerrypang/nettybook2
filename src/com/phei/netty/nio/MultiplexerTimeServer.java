264266d508f19cfbdc7fdccd884757c06a412c73
pangguowei <pangguowei@yxqiche.com>
2020-12-31 11:00:48 +0800

    格式化代码，添加注释

--- src/com/phei/netty/nio/MultiplexerTimeServer.java
+++ src/com/phei/netty/nio/MultiplexerTimeServer.java
@@ -44,21 +44,27 @@ public class MultiplexerTimeServer implements Runnable {
      * @param port
      */
     public MultiplexerTimeServer(int port) {
-	try {
-	    selector = Selector.open();
-	    servChannel = ServerSocketChannel.open();
-	    servChannel.configureBlocking(false);
-	    servChannel.socket().bind(new InetSocketAddress(port), 1024);
-	    servChannel.register(selector, SelectionKey.OP_ACCEPT);
-	    System.out.println("The time server is start in port : " + port);
-	} catch (IOException e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
+		try {
+			// 创建多路复用器selector
+		    selector = Selector.open();
+		    // 打开ServerSocketChannel 用于监听客户端连接，是所有客户端连接的父管道
+		    servChannel = ServerSocketChannel.open();
+		    // 将ServerSocketChannel 设置为异步非阻塞 
+		    servChannel.configureBlocking(false);
+		    // 设置backlog 为1024，绑定端口
+		    servChannel.socket().bind(new InetSocketAddress(port), 1024);
+		    // 将ServerSocketChannel 注册到 selector，监听ACCEPT操作
+		    servChannel.register(selector, SelectionKey.OP_ACCEPT);
+		    System.out.println("The time server is start in port : " + port);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		    // 如果资源初始化失败则退出
+		    System.exit(1);
+		}
     }
 
     public void stop() {
-	this.stop = true;
+    	this.stop = true;
     }
 
     /*
@@ -67,87 +73,106 @@ public class MultiplexerTimeServer implements Runnable {
      * @see java.lang.Runnable#run()
      */
     @Override
-    public void run() {
-	while (!stop) {
-	    try {
-		selector.select(1000);
-		Set<SelectionKey> selectedKeys = selector.selectedKeys();
-		Iterator<SelectionKey> it = selectedKeys.iterator();
-		SelectionKey key = null;
-		while (it.hasNext()) {
-		    key = it.next();
-		    it.remove();
-		    try {
-			handleInput(key);
-		    } catch (Exception e) {
-			if (key != null) {
-			    key.cancel();
-			    if (key.channel() != null)
-				key.channel().close();
+	public void run() {
+    	// 在无线循环体内轮询selector准备就绪的key
+		while (!stop) {
+			try {
+				// selector 休眠时间为1s，无论是否有读写事件，每隔一秒都被唤醒
+				selector.select(1000);
+				// selector返回就绪状态channel的SelectionKey集合 
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				// 迭代SelectionKey集合
+				Iterator<SelectionKey> it = selectedKeys.iterator();
+				SelectionKey key = null;
+				while (it.hasNext()) {
+					key = it.next();
+					it.remove();
+					try {
+						// 进行异步读写操作
+						handleInput(key);
+					} catch (Exception e) {
+						if (key != null) {
+							key.cancel();
+							if (key.channel() != null)
+								key.channel().close();
+						}
+					}
+				}
+			} catch (Throwable t) {
+				t.printStackTrace();
 			}
-		    }
 		}
-	    } catch (Throwable t) {
-		t.printStackTrace();
-	    }
+
+		// 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
+		if (selector != null) {
+			try {
+				selector.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
-	// 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
-	if (selector != null)
-	    try {
-		selector.close();
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-    }
-
-    private void handleInput(SelectionKey key) throws IOException {
-
-	if (key.isValid()) {
-	    // 处理新接入的请求消息
-	    if (key.isAcceptable()) {
-		// Accept the new connection
-		ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
-		SocketChannel sc = ssc.accept();
-		sc.configureBlocking(false);
-		// Add the new connection to the selector
-		sc.register(selector, SelectionKey.OP_READ);
-	    }
-	    if (key.isReadable()) {
-		// Read the data
-		SocketChannel sc = (SocketChannel) key.channel();
-		ByteBuffer readBuffer = ByteBuffer.allocate(1024);
-		int readBytes = sc.read(readBuffer);
-		if (readBytes > 0) {
-		    readBuffer.flip();
-		    byte[] bytes = new byte[readBuffer.remaining()];
-		    readBuffer.get(bytes);
-		    String body = new String(bytes, "UTF-8");
-		    System.out.println("The time server receive order : "
-			    + body);
-		    String currentTime = "QUERY TIME ORDER"
-			    .equalsIgnoreCase(body) ? new java.util.Date(
-			    System.currentTimeMillis()).toString()
-			    : "BAD ORDER";
-		    doWrite(sc, currentTime);
-		} else if (readBytes < 0) {
-		    // 对端链路关闭
-		    key.cancel();
-		    sc.close();
-		} else
-		    ; // 读到0字节，忽略
-	    }
+	private void handleInput(SelectionKey key) throws IOException {
+		if (key.isValid()) {
+			// 处理新接入的请求消息
+			if (key.isAcceptable()) {
+				// Accept the new connection
+				ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+				// 通过 ServerSocketChannel 的 accept 接收客户端连接，并创建 SocketChannel（完成TCP三次握手，和客户端建立物理连接）
+				SocketChannel sc = ssc.accept();
+				// SocketChannel 设置为异步非阻塞
+				sc.configureBlocking(false);
+				// 将客户端连接注册到 selector，监听读操作，可读取客户端发送的消息
+				sc.register(selector, SelectionKey.OP_READ);
+			}
+			
+			if (key.isReadable()) {
+				// Read the data
+				SocketChannel sc = (SocketChannel) key.channel();
+				// 创建一个1024k 缓冲区大小的 ByteBuffer 
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				// 异步读取缓冲区数据， read 为非阻塞 
+				int readBytes = sc.read(readBuffer);
+				if (readBytes > 0) {
+					// 大于0 ，读到了字节，对字节进行编解码
+					// 将缓冲区当前limit设置为position 设为0，用于对缓冲区的读操作
+					readBuffer.flip();
+					// 根据缓冲区可读字节大小创建字节数组
+					byte[] bytes = new byte[readBuffer.remaining()];
+					// 将缓冲区可读字节拷贝到字节数组
+					readBuffer.get(bytes);
+					// 解码
+					String body = new String(bytes, "UTF-8");
+					System.out.println("The time server receive order : " + body);
+					String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
+							? new java.util.Date(System.currentTimeMillis()).toString()
+							: "BAD ORDER";
+					// 发送应答消息给客户端
+					doWrite(sc, currentTime);
+					
+				}  else if (readBytes < 0) {
+					// 对端链路关闭
+					key.cancel();
+					sc.close();
+					
+				}  else {
+					; // 读到0字节，忽略
+				}
+			}
+		}
 	}
-    }
 
-    private void doWrite(SocketChannel channel, String response)
-	    throws IOException {
-	if (response != null && response.trim().length() > 0) {
-	    byte[] bytes = response.getBytes();
-	    ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
-	    writeBuffer.put(bytes);
-	    writeBuffer.flip();
-	    channel.write(writeBuffer);
+	private void doWrite(SocketChannel channel, String response) throws IOException {
+		if (response != null && response.trim().length() > 0) {
+			byte[] bytes = response.getBytes();
+			// 根据字节数组创建 ByteBuffer
+			ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+			// 将字节数组复制到缓冲区
+			writeBuffer.put(bytes);
+			writeBuffer.flip();
+			// SocketChannel 将缓冲区字节数组发送出去
+			channel.write(writeBuffer);
+		}
 	}
-    }
 }
--- src/com/phei/netty/nio/TimeClientHandle.java
+++ src/com/phei/netty/nio/TimeClientHandle.java
@@ -31,125 +31,149 @@ import java.util.Set;
  */
 public class TimeClientHandle implements Runnable {
 
-    private String host;
-    private int port;
+	private String host;
+	private int port;
 
-    private Selector selector;
-    private SocketChannel socketChannel;
+	private Selector selector;
+	private SocketChannel socketChannel;
 
-    private volatile boolean stop;
+	private volatile boolean stop;
 
-    public TimeClientHandle(String host, int port) {
-	this.host = host == null ? "127.0.0.1" : host;
-	this.port = port;
-	try {
-	    selector = Selector.open();
-	    socketChannel = SocketChannel.open();
-	    socketChannel.configureBlocking(false);
-	} catch (IOException e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Runnable#run()
-     */
-    @Override
-    public void run() {
-	try {
-	    doConnect();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
-	while (!stop) {
-	    try {
-		selector.select(1000);
-		Set<SelectionKey> selectedKeys = selector.selectedKeys();
-		Iterator<SelectionKey> it = selectedKeys.iterator();
-		SelectionKey key = null;
-		while (it.hasNext()) {
-		    key = it.next();
-		    it.remove();
-		    try {
-			handleInput(key);
-		    } catch (Exception e) {
-			if (key != null) {
-			    key.cancel();
-			    if (key.channel() != null)
-				key.channel().close();
-			}
-		    }
+	public TimeClientHandle(String host, int port) {
+		this.host = host == null ? "127.0.0.1" : host;
+		this.port = port;
+		try {
+			// 创建多路复用器selector
+			selector = Selector.open();
+			// 打开 SocketChannel 绑定客户端本地地址
+			socketChannel = SocketChannel.open();
+			// 将 SocketChannel 设置为非阻塞模式
+			socketChannel.configureBlocking(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
 		}
-	    } catch (Exception e) {
-		e.printStackTrace();
-		System.exit(1);
-	    }
 	}
 
-	// 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
-	if (selector != null)
-	    try {
-		selector.close();
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		try {
+			doConnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		// 在无线循环体内轮询selector准备就绪的key
+		while (!stop) {
+			try {
+				// selector 休眠时间为1s，无论是否有读写事件，每隔一秒都被唤醒
+				selector.select(1000);
+				// selector返回就绪状态channel的SelectionKey集合 
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				// 迭代SelectionKey集合
+				Iterator<SelectionKey> it = selectedKeys.iterator();
+				SelectionKey key = null;
+				while (it.hasNext()) {
+					key = it.next();
+					it.remove();
+					try {
+						// 进行异步读写操作
+						handleInput(key);
+					} catch (Exception e) {
+						if (key != null) {
+							key.cancel();
+							if (key.channel() != null)
+								key.channel().close();
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
 
-    }
+		// 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源
+		if (selector != null) {
+			try {
+				selector.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-    private void handleInput(SelectionKey key) throws IOException {
-
-	if (key.isValid()) {
-	    // 判断是否连接成功
-	    SocketChannel sc = (SocketChannel) key.channel();
-	    if (key.isConnectable()) {
-		if (sc.finishConnect()) {
-		    sc.register(selector, SelectionKey.OP_READ);
-		    doWrite(sc);
-		} else
-		    System.exit(1);// 连接失败，进程退出
-	    }
-	    if (key.isReadable()) {
-		ByteBuffer readBuffer = ByteBuffer.allocate(1024);
-		int readBytes = sc.read(readBuffer);
-		if (readBytes > 0) {
-		    readBuffer.flip();
-		    byte[] bytes = new byte[readBuffer.remaining()];
-		    readBuffer.get(bytes);
-		    String body = new String(bytes, "UTF-8");
-		    System.out.println("Now is : " + body);
-		    this.stop = true;
-		} else if (readBytes < 0) {
-		    // 对端链路关闭
-		    key.cancel();
-		    sc.close();
-		} else
-		    ; // 读到0字节，忽略
-	    }
 	}
 
-    }
+	private void handleInput(SelectionKey key) throws IOException {
+		if (key.isValid()) {
+			// 判断是否连接成功
+			SocketChannel sc = (SocketChannel) key.channel();
+			// 接收connect事件进行处理
+			if (key.isConnectable()) {
+				// 判断连接结果，如果连接成功，注册读事件到 selector
+				if (sc.finishConnect()) {
+					sc.register(selector, SelectionKey.OP_READ);
+					doWrite(sc);
+				} else
+					System.exit(1);// 连接失败，进程退出
+			}
+			
+			if (key.isReadable()) {
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				// 异步读取缓冲区数据
+				int readBytes = sc.read(readBuffer);
+				if (readBytes > 0) {
+					// 大于0 ，读到了字节，对字节进行编解码
+					// 将缓冲区当前limit设置为 position 设为0，用于对缓冲区的读操作
+					readBuffer.flip();
+					// 根据缓冲区可读字节大小创建字节数组
+					byte[] bytes = new byte[readBuffer.remaining()];
+					// 将缓冲区可读字节拷贝到字节数组
+					readBuffer.get(bytes);
+					String body = new String(bytes, "UTF-8");
+					System.out.println("Now is : " + body);
+					this.stop = true;
+					
+				} else if (readBytes < 0) {
+					// 对端链路关闭
+					key.cancel();
+					sc.close();
+				} else {
+					; // 读到0字节，忽略
+				}
+			}
+		}
 
-    private void doConnect() throws IOException {
-	// 如果直接连接成功，则注册到多路复用器上，发送请求消息，读应答
-	if (socketChannel.connect(new InetSocketAddress(host, port))) {
-	    socketChannel.register(selector, SelectionKey.OP_READ);
-	    doWrite(socketChannel);
-	} else
-	    socketChannel.register(selector, SelectionKey.OP_CONNECT);
-    }
+	}
 
-    private void doWrite(SocketChannel sc) throws IOException {
-	byte[] req = "QUERY TIME ORDER".getBytes();
-	ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
-	writeBuffer.put(req);
-	writeBuffer.flip();
-	sc.write(writeBuffer);
-	if (!writeBuffer.hasRemaining())
-	    System.out.println("Send order 2 server succeed.");
-    }
+	private void doConnect() throws IOException {
+		// 异步连接服务端
+		if (socketChannel.connect(new InetSocketAddress(host, port))) {
+			// 连接成功（获取TCP的ACK应答，建立了物理连接），则注册到多路复用器上读应答
+			socketChannel.register(selector, SelectionKey.OP_READ);
+			// 发送请求消息
+			doWrite(socketChannel);
+		} else {
+			// 没连接成功, 向多路复用器注册，监听CONNECT事件（监听TCP的ACK应答）
+			socketChannel.register(selector, SelectionKey.OP_CONNECT);
+		}
+	}
+
+	private void doWrite(SocketChannel sc) throws IOException {
+		byte[] req = "QUERY TIME ORDER".getBytes();
+		ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+		writeBuffer.put(req);
+		writeBuffer.flip();
+		sc.write(writeBuffer);
+		if (!writeBuffer.hasRemaining()) {
+			System.out.println("Send order 2 server succeed.");
+		}
+	}
 
 }
